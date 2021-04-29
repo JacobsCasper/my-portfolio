da1c6a01ec9080c263c2764bd20795943bd581d4
Casper <casper.jacobs@student.pxl.be>
2021-04-29 20:29:18 +0200

    added https

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
+
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
 //Install express server
 const express = require('express');
 const path = require('path');
@@ -7,9 +16,10 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/my-portfolio'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/my-portfolio/'}),
-);
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/my-portfolio/'}
+  );
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
